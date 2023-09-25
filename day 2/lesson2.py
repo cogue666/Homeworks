b73210e1ea488724acca0df385947cd50f3f894e
cogue666 <hidn2025@gmail.com>
2023-09-25 17:54:43 +0400

    first homework

--- day 2/lesson2.py
+++ day 2/lesson2.py
@@ -1,1 +1,31 @@
-print("this is day2")
+name = "Tato" #ეს არის str (string) ტიპის ცვლადი
+#name არის ცვლადი
+# = მნიშვნელობის მიმნიჭებელი სიმბოლო ცვლადზე
+# "Tato" არის ცვლადისთვის მნიშვნელობა
+surname = "Chogovadze"
+
+age = "20" #ეს არის int (integer) მთელი რიცხვი
+height = "1.87" #ეს არის float ტიპის ცვლადი (ათწილადი)
+print(name)
+print(surname)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+#boolean (booL) ტიპის ცვლადი
+knows_programming = True #True ან False
+is_ugly = False #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+is_smart = True 
+isUgly = False #ჯავასკრიპტული cameLcase
+
+
+
+print(age)
+print(height)
+
+print(name + " " + surname + " ", age + " " + height)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#ინტეგერი არის მთელი რიცხვი
+
+
+
+
+
