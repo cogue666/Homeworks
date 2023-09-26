9d5033b92ab5bcf2d66a03b194460463b525d971
cogue666 <hidn2025@gmail.com>
2023-09-26 23:00:49 +0400

    third lesson

---
+++ day 3/lesson 3.py
@@ -0,0 +1,61 @@
+print(2.71)
+
+print("I am a string")
+
+#ბრჭყალებში რაცარის ყველაფერი არის სტრინგი
+
+print_HP = 80
+player_Mana = 90
+spell_mana = 20
+
+print ('ubralod')
+
+print(7+3)
+
+print(7+3)
+print(10 - 5)
+print(5*3)
+print(10 / 2)
+# / / აცილებს წერტილს იქით ყველაფერს.
+# / გაძლევს ზუსტ პასუხს
+# ბრჭყალები რასაც აქვს ყველაფერი არის სტრინგი
+# Variables - ცვლადები
+
+
+#შექმენით თქვენი ოჯახის წევრების ასაკებისგან ცვლადები
+#დაპრინტეთ  ასაკები 5 წლის შემდეგ
+
+
+age=21 
+print(age)
+print(age + 5)
+
+age=1.5
+print(age)
+print(age+5)
+
+age=38
+print(age)
+print(age+5)
+
+
+# mom_age = 60
+# dad_age = 73
+# my_age = 105
+
+mom_age = 37
+my_age = 20
+
+print(my_age + 5)
+print(mom_age + 5)
+
+
+mom_age += 5
+my_age += 5
+
+age=37
+age+=5
+
+
+
+
